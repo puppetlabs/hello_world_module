5ffb7c537f6b4a9470ac124167b3f7377425c473
Jain Waldrip <jain.waldrip@perforce.com>
2024-09-12 19:41:54 -0500

    Add bundler check to scripts/install_gems.sh

--- scripts/install_gems.sh
+++ scripts/install_gems.sh
@@ -2,6 +2,12 @@
 
 # Copyright 2024. Puppet, Inc., a Perforce company. 
 
+# You need to have bundler installed for this to work
+if [gem list -i bundler]; then
+  echo "Bundler is not installed. Please install it using 'gem install bundler'"
+  exit
+fi
+
 # Ensure we install all the development gems
 bundle config set development true
 
